2532bd7f917954bac40497f94c76f7590f8800e4
Jaden <jxd0901@gmail.com>
2020-07-07 19:53:48 +0000

    Creation of secretPage function and funfact html page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,94 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+/*
+ In order to access the secret page you have to solve a randomly generated equation.
+ Upon the input of a correct answer a button will appear, linking the user to the page.
+ */
+function secretPage(){
+  //Generates a number between 1 and 200 for both numbers.
+  this.a = Math.floor((Math.random()*200)+100);
+  this.b = Math.floor((Math.random()*200)+100);
+
+
+  //If b is a greater number we flip the values
+  if(this.b > this.a){
+    var hold =this.b;
+    this.b = this.a;
+    this.a = hold;
+  }
+
+  // This section of code includes the possible equations to solve
+  this.div = function(){
+    return this.a / this.b;
+  }
+  this.sub = function(){
+    return this.a - this.b;
+  }
+  this.add = function(){
+    return this.a + this.b;
+  } 
+  this.mul = function(){
+    return this.a * this.b;   
+  }
+  
+  //Checks if the answer givin is correct
+  let isCorrect = false;
+  
+  //Generates which equation option we choose;
+  let choice = Math.floor((Math.random()*4));
+  
+  
+  let input = 0;
+  let answer = 0;
+
+//choice determines which equation the user gets 
+  switch(choice){
+      case 0:
+        answer = this.add();
+        input = prompt(this.a +' + ' + this.b + '?',0);
+        if(input == answer){ 
+          isCorrect = true;
+        }
+        break;
+    
+      case 1:     
+        answer = this.sub();
+        input = prompt(this.a +' - ' + this.b + '?',0);
+        if(input == answer){  
+          isCorrect = true;
+        }
+        break;
+    
+      case 2:
+        answer = this.mul();
+        input = prompt(this.a +' * ' + this.b + '?',0);
+        if(input == answer){ 
+          isCorrect = true;
+        }
+        break;
+    
+      case 3:
+        answer = Math.round(this.div());
+        input = prompt(this.a +' / ' + this.b + '? Rounded to the nearest Integer',0);
+        if(input == answer){
+          isCorrect = true;
+        }
+       break;
+
+      default:
+        alert("Something went wrong :( Refresh the page and try again!");
+    }
+   
+    //The button to access the secret page is hidden until the user enters a correct answer 
+    let secret = document.getElementById('secret-button');
+    if(isCorrect){
+        alert('Congratulations! You have unlocked my Secret.');
+        secret.style.display = 'block';
+    }else{
+        alert("Sorry that's an incorrect answer. Better luck next time!");
+    }
+
+}
+
